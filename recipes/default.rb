8de4cf21b96241c6a31f0b638ff0ff57333a019b
faberyx <fabrizio.catitti@gmail.com>
2016-05-07 10:57:26 +0200

    added nginx configuration

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,24 @@
 #
 
 include_recipe "nodejs"
+include_recipe "nginx::default"
 
- 
-include_recipe "nxinx::default"
+begin
+  r = resources(:template => "/etc/nginx/sites-enabled/000-default")
+  r.cookbook('node-test')
+  r.source('default-sire.erb')
+rescue Chef::Exceptions::ResourceNotFound
+  Chef::Log.warn "could not find template to override!"
+end
+
+directory '/var/www/nginx-default' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  action :create
+end
+cookbook_file "/var/www/nginx-default/index.html" do
+  source "index.html"
+  mode "0644"
+end
